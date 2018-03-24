88f48303f263b6e80122d7dd63603ff8438e0309
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2018-03-24 00:23:33 -0500

    React only based Youtube Search App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyAvdblF6vgn65_4MrRb-2MHuD0XeEKad3E'
 
 // Create a new component with ES6 syntax
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null 
+        };
+
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            //Set the State (Will cause component to Re-render)
+            this.setState({ 
+                videos: videos, 
+                selectedVideo: videos[0]
+            });
+        });
+
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // take the components' and put it on the page (in the DOM) with a reference to page element
